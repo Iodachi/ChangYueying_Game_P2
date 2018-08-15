fe5c1e79a9fd2d70f3787a1dd67e7fcd42a552c6
iodachi <a547601908@gmail.com>
2018-08-15 18:52:59 +1200

    Initial commit

---
+++ Assets/Objects/Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	private float speed = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float axisX = Input.GetAxis ("Horizontal");
+
+		transform.Translate (new Vector2(axisX, 0) * Time.deltaTime * speed);
+	}
+}
